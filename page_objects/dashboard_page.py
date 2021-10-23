f29a96a65735bcfcc204d0650435bdbb3b0127be
Olena Piankova <el.piankova@gmail.com>
2021-10-23 11:38:53 +0300

    Allure report

--- page_objects/dashboard_page.py
+++ page_objects/dashboard_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.common.by import By
 
 from custom_wait_conditions import presence_of_N_elements_located
@@ -27,6 +28,7 @@ class DashboardPage(InternalPage):
         new_list = [PostBlock(e) for e in el_posts]
         return new_list
 
+    @allure.step("THEN a new post block appears before old table of posts")
     def wait_new_post_appear(self, number_of_posts_before):
         # TODO: extract locators
         # Wait new post appears
@@ -37,11 +39,13 @@ class DashboardPage(InternalPage):
         el_post_text = el_posts_new[0].find_element(By.CLASS_NAME, "ow_newsfeed_content")
         return el_post_text
 
+    @allure.step("WHEN I add a new post with '{input_text}' in Dashboard page")
     def create_new_post(self, input_text):
         # Create new post
         self.el_post_textfield.send_keys(input_text)
         self.el_send_button.click()
 
+    @allure.step("GIVEN initial amount of post in Oxwall database")
     def count_posts(self):
         # Amount of posts
         number_of_posts_before = len(self.el_posts)
@@ -49,4 +53,13 @@ class DashboardPage(InternalPage):
 
     def is_this_page(self):
         return self.el_page_title.text == "MY DASHBOAR"
+
+    @allure.step("THEN this post block has this '{text}' and author as '{user}' and time \"within 1 minute\"")
+    def verify_post(self, text, user):
+        new_post = self.el_posts[0]
+        # Verify text and user of new post
+        assert new_post.el_text.text == text
+        assert new_post.el_user.text == user.real_name
+        assert new_post.el_time.text == "within 1 minute"
+
 
--- tests/test_post_create.py
+++ tests/test_post_create.py
@@ -1,4 +1,6 @@
 import time
+
+import allure
 import pytest
 import json
 from conftest import PROJECT_DIR
@@ -14,25 +16,30 @@ with open(filename, encoding="utf-8") as f:
 post_data_list.append(random_string(enter=True))
 
 
+@allure.feature("Post feature")
+@allure.title("Post create test")
+@allure.story("Create text post (without photos)")
 @pytest.mark.smoke
 @pytest.mark.nondestructive
 @pytest.mark.parametrize("input_text", post_data_list)
 def test_create_post(driver, sign_in_user, input_text, db):
     # input_text = "new post typing"
     dashboard_page = DashboardPage(driver)
-    dashboard_page.el_post_textfield.clear()
     number_of_posts_before = dashboard_page.count_posts()
+    dashboard_page.el_post_textfield.clear()
     dashboard_page.create_new_post(input_text)
     dashboard_page.wait_new_post_appear(number_of_posts_before)
+
     text_in_db = db.get_last_text_post()
     assert text_in_db.replace("\r\n", "\n") == input_text
 
-    new_post = dashboard_page.el_posts[0]
-    # Verify text and user of new post
-    assert new_post.el_text.text == input_text
-    assert new_post.el_user.text == sign_in_user.real_name
-    assert new_post.el_time.text == "within 1 minute"
-    # TODO el_post_user == sign_in_user["username"]
+    dashboard_page.verify_post(input_text, sign_in_user)
+    # new_post = dashboard_page.el_posts[0]
+    # # Verify text and user of new post
+    # assert new_post.el_text.text == input_text
+    # assert new_post.el_user.text == sign_in_user.real_name
+    # assert new_post.el_time.text == "within 1 minute"
+
 
 
 filename = os.path.join(PROJECT_DIR, "data", "input_output.json")
@@ -41,6 +48,9 @@ with open(filename, encoding="utf-8") as f:
     post_data_list = json.load(f)
 
 
+@allure.feature("Post feature")
+@allure.title("Post create test 2")
+@allure.story("Create text post (without photos) 2")
 @pytest.mark.parametrize("input_text,output_text", post_data_list)
 def test_create_post_2(driver, sign_in_user, input_text, output_text):
     # input_text = "new post typing"
@@ -57,6 +67,9 @@ def test_create_post_2(driver, sign_in_user, input_text, output_text):
     # TODO el_post_user == sign_in_user["username"]
 
 
+@allure.feature("Post feature")
+@allure.title("First post create")
+@allure.story("Create first post ")
 def test_add_like_to_first_post(driver, sign_in_user):
     dashboard_page = DashboardPage(driver)
     first_post = dashboard_page.el_posts[0]
